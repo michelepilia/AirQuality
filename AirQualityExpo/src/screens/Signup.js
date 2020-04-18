a7116028543495ac79638eb5dfd7c2e97cf2c363
michelepilia <michele.pilia@outlook.it>
2020-04-18 18:16:00 +0200

    Add Signup backend

--- AirQualityExpo/src/screens/Signup.js
+++ AirQualityExpo/src/screens/Signup.js
@@ -4,27 +4,116 @@ import MaterialRightIconTextbox from "../components/MaterialRightIconTextbox";
 import MaterialStackedLabelTextbox from "../components/MaterialStackedLabelTextbox";
 import { TextField } from 'react-native-material-textfield';
 import PasswordInputText from 'react-native-hide-show-password-input';
+import RNPickerSelect from 'react-native-picker-select';
+import DatePicker from 'react-native-datepicker';
 
 class Signup extends Component {
 
   state = {
-      username: '',
       email: '',
+      firstName: '',
+      lastName: '',
       password: '',
-      confirmPassword: ''
+      gender: '',
+      birthDay: '',
+      
   };
+    today = {
+      year: new Date().getFullYear(),
+      month: new Date().getMonth()+1,
+      day: new Date().getDate(),
+  }
 
-  handleUsername = (text) => {
-      this.setState({ username: text })
+  url = "https://polimi-dima-server.herokuapp.com/api";
+
+  confirmPassword= '';
+
+  handleFirstName = (text) => {
+    this.setState({ firstName: text })
    };
 
-   handleEmail = (text) => {
-      this.setState({ email: text })
-   }
+  handleEmail = (text) => {
+    this.setState({ email: text })
+  }
+  handleLastName = (text) => {
+    this.setState({ lastName: text })
+  }
+  handleGender = (text) => {
+    this.setState({ gender: text })
+  }
+  handlePassword = (text) => {
+    this.setState({ password: text })
+  }
+  handleConfirmPassword = (text) => {
+    this.confirmPassword = text;
+  }
    
   formatText = (text) => {
     return text.replace(/[^+\d]/g, '');
   };
+
+  registerFunction(){
+    console.log("confirm: " + this.confirmPassword + "Password: " + this.state.password);
+    passwordMatch = ((this.state.password)==(this.confirmPassword));
+    passwordLength = ((this.state.password).length >= 4);
+    if(passwordMatch&&passwordLength){
+      return fetch(this.url+"/user/register", {
+        method: "post",
+        headers:{
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: this.state.email,
+          firstName: this.state.firstName,
+          lastName: this.state.lastName,
+          password: this.state.password,
+          gender: this.state.gender,
+          birthDay: this.state.birthDay
+        }),
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.status == "200"){
+          this.props.navigation.navigate("Login");
+        }
+        else {
+          alert("Invalid response");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    } else {
+      if (!passwordMatch) {alert("Password and Confirm Password must be equal");}
+      if (!passwordLength) {alert("Password must be longer than 4 characters");}
+    }
+  }
+
+  /*doLogin(email, password){
+    return fetch(this.url+"/user/login", {
+      method: "post",
+      headers:{
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify( {email: {email}, password: {password}} ),
+    })
+    .then((response) => {
+      if (response.status == "200"){
+        return (response.json());
+      }
+      else {
+        alert("Invalid response");
+      }
+    })
+    .then((json)=>{
+      this.props.navigation.navigate("Home", {token: json.token});
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }*/
 
   render(){
     return (
@@ -42,7 +131,68 @@ class Signup extends Component {
         </View>
         <Text style={styles.airQuality1}>Air Quality</Text>
 
-        <View style={{margin: 20}}>
+        <View style={styles.inputView}>
+          <TextField style = {styles.inputUser}
+               underlineColorAndroid = "transparent"
+               autoCapitalize = "none"
+               onChangeText = {this.handleFirstName}
+               formatText={this.formatText}
+               label="First Name"/>
+        </View>
+
+        <View style={styles.inputView}>
+          <TextField style = {styles.inputUser}
+               underlineColorAndroid = "transparent"
+               autoCapitalize = "none"
+               onChangeText = {this.handleLastName}
+               formatText={this.formatText}
+               label="Last Name"/>
+        </View>
+
+        <View style={styles.inputView}>
+          <RNPickerSelect
+            placeholder = {{
+              label: 'Gender',
+              value: null,
+              color: '#9EA0A4',
+            }}
+            onValueChange={this.handleGender}
+            items={[
+                { label: 'Male', value: 'male' },
+                { label: 'Female', value: 'female' },
+                { label: 'Other', value: 'other' },
+            ]}
+          />
+        </View>
+
+        <View style={styles.inputView}>
+          <DatePicker
+            style={styles.dateInput}
+            date={this.state.birthDay}
+            mode="date"
+            placeholder="Birthday"
+            format="YYYY-MM-DD"
+            minDate={(this.today.year - 120)+"-01-01"}
+            maxDate={(this.today.year + '-' + this.today.month + '-' + this.today.day)}
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            customStyles={{
+              dateIcon: {
+                position: 'absolute',
+                left: 0,
+                top: 4,
+                marginLeft: 0
+              },
+              dateInput: {
+                marginLeft: 36
+              }
+            // ... You can check the source to find the other keys.
+            }}
+            onDateChange={(date) => {this.setState({birthDay: date})}}
+          />
+        </View>
+
+        <View style={styles.inputView}>
           <TextField style = {styles.inputUser}
                underlineColorAndroid = "transparent"
                autoCapitalize = "none"
@@ -51,31 +201,23 @@ class Signup extends Component {
                label="Email"/>
         </View>
 
-        <View style={{margin: 20}}>
-          <TextField style = {styles.inputUser}
-               underlineColorAndroid = "transparent"
-               autoCapitalize = "none"
-               onChangeText = {this.handleUsername}
-               formatText={this.formatText}
-               label="Username"/>
-        </View>
 
-        <View style={{margin: 20}}>
+        <View style={styles.inputView}>
           <PasswordInputText
             value={this.state.password}
-            onChangeText={ (password) => this.setState({ password }) }/>
+            onChangeText={this.handlePassword}/>
         </View>
 
-        <View style={{margin: 20}}>
+        <View style={styles.inputView}>
           <PasswordInputText
-            value={this.state.confirmPassword}
+            value={this.confirmPassword}
             label = "Confirm Password"
-            onChangeText={ (confirmPassword) => this.setState({ confirmPassword }) }/>
+            onChangeText={this.handleConfirmPassword}/>
         </View>
 
 
         <TouchableOpacity
-          onPress={() => this.props.navigation.navigate("Home")}
+          onPress={() => this.registerFunction()}
           style={styles.button1}>
           <Text style={styles.text}>Signup</Text>
         </TouchableOpacity>
@@ -173,7 +315,10 @@ const styles = StyleSheet.create({
     marginTop: 30,
     marginLeft: 10,
     marginRight: 21
-  }
+  },
+  inputView:{
+    margin: 5,
+  },
 });
 
 export default Signup;
