eb88a93bdb59173fc2cc3d7ca52a339edf13d237
Antonino96 <10546001@polimi.it>
2020-05-08 16:25:28 +0200

    Added startDate endDate request

--- AirQualityExpo/src/screens/Stats.js
+++ AirQualityExpo/src/screens/Stats.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
 import { TextField } from 'react-native-material-textfield';
+import DatePicker from 'react-native-datepicker';
 
 class Stats extends Component{
 
-  state = {
-    token: '',
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: '',
+      startDate: '',
+      endDate: '',
+    }
+    this.retrieveDataByDate = this.retrieveDataByDate.bind(this);
+    this.retrieveData = this.retrieveData.bind(this);
+    this.today={
+      year: new Date().getFullYear(),
+      month: new Date().getMonth()+1,
+      day: new Date().getDate(),
+    }
+    this.urlDate = "https://polimi-dima-server.herokuapp.com/api/data/findByDate?startDate=2020-04-15T09:00:00Z&endDate=2020-04-16T23:00:00Z";
+    this.url = "https://polimi-dima-server.herokuapp.com/api/data?offset=0&limit=1";
+   // this.startDate = this.today;
+   // this.endDate = this.today;
   }
 
-  urlSaveData = "https://polimi-dima-server.herokuapp.com/api/data?offset=0&limit=1";
-
   retrieveData(){
-    return fetch(this.urlSaveData)
+    return fetch(this.url)
     .then((response) => {
       console.log("RESPONSE CODE: "+response.status);
       if (response.status == "200"){
@@ -27,13 +42,46 @@ class Stats extends Component{
     });
   }
 
+  retrieveDataByDate(){
+    return fetch(this.urlDate)
+    .then((response) => {
+      console.log("RESPONSE CODE: "+response.status);
+      if (response.status == "200"){
+        return (response.json());
+      }
+      else {
+        alert("Invalid response");
+      }
+    })
+    .then((response) => console.log(response))
+    .catch((error) => {
+      console.error(error);
+    });
+
+  }
+
   componentDidMount(){
     const { params } = this.props.navigation.state;
     const token1 = params ? params.token : null;
     this.setState({token:token1});
+    console.log("MOUNTING HISTORICAL DATA COMPONENT");
+    
+  }
+
+  componentDidUpdate() {
+    console.log("UPDATING")
+    console.log("This.state.startDate = "+this.state.startDate);
+    if(this.state.startDate!=''){
+      console.log("This.state.endDate = "+this.state.endDate);
+      if(this.state.endDate!='' && this.state.endDate>this.state.startDate){
+        this.retrieveDataByDate();
+      }
+    }
   }
 
   render(){
+    console.log("rendering HISTORICAL DATA COMPONENT");
+
     return (
       <View style={styles.container}>
 
@@ -63,17 +111,79 @@ class Stats extends Component{
 
         </View>
 
-        <TouchableOpacity
-          onPress={() => {
-            console.log("TOKEN: "+this.state.token);
-            this.retrieveData();
-          }
-          }
-          style={styles.viewMapBtn}>
-          <Text style={styles.viewMapLabel}>Retrieve Data</Text>
-        </TouchableOpacity>
-
         <Text style={styles.title}>Historical Data</Text>
+
+
+        <View 
+          style={{
+          marginTop:20,
+          marginLeft: 20,
+          }}>
+          <Text >Get data by date</Text>
+          <View style={{
+            marginTop:10,
+            flexDirection:"row",
+            marginLeft:'auto',
+            marginRight:'auto',
+
+            }}>
+            <DatePicker
+              style={styles.dateInput}
+              date={this.state.startDate}
+              mode="date"
+              placeholder="Start Date"
+              format="YYYY-MM-DD"
+              minDate={(this.today.year - 120)+"-01-01"}
+              maxDate={(this.today.year + '-' + this.today.month + '-' + this.today.day)}
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
+              customStyles={{
+                dateIcon: {
+                  position: 'absolute',
+                  left: 0,
+                  top: 4,
+                  marginLeft: 0
+                },
+                dateInput: {
+                  marginLeft: 35,
+                  height:30,
+                  width:70
+                  
+                }
+              }}
+              onDateChange={(date) => {this.setState({startDate: date})}}
+            />
+            <DatePicker
+              style={styles.dateInput}
+              date={this.state.endDate}
+              mode="date"
+              placeholder="End Date"
+              format="YYYY-MM-DD"
+              minDate={(this.today.year - 120)+"-01-01"}
+              maxDate={(this.today.year + '-' + this.today.month + '-' + this.today.day)}
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
+              customStyles={{
+                dateIcon: {
+                  position: 'absolute',
+                  left: 0,
+                  top: 4,
+                  marginLeft: 0
+                },
+                dateInput: {
+                  marginLeft: 35,
+                  height:30,
+                  width:70
+                }
+              }}
+              onDateChange={(date) => {this.setState({endDate: date})}}
+            />
+
+          </View>
+          
+
+        </View>
+
 
       </View>
     );
@@ -106,22 +216,17 @@ const styles = StyleSheet.create({
     alignSelf: "flex-end",
     marginTop: 25
   },
-  viewMapBtn: {
-    width: 305,
-    height: 93,
+  retrieveDataBtn: {
+    width: 80,
+    height: 30,
     backgroundColor: "rgba(255,0,0,1)",
-    marginTop: 50,
-    marginLeft: 35
+    marginLeft:10
   },
   viewMapLabel: {
-    width: 305,
-    height: 93,
     color: "rgba(255,255,255,1)",
-    fontSize: 30,
     fontFamily: "roboto-regular",
-    lineHeight: 93,
     letterSpacing: 0,
-    textAlign: "center"
+    textAlign: "center",
   },
   readDataBtn: {
     width: 305,
